49ef0a316af55c56f6d6f29934e3ef9e19e5b8a4
Raymond Brown <daimyoplays@gmail.com>
2022-07-20 14:52:17 -0500

    added edit todos

--- project/src/components/Home.js
+++ project/src/components/Home.js
@@ -6,7 +6,7 @@ import {
 } from "@react-query-firebase/firestore";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, setDoc } from "firebase/firestore";
 
 function Home() {
   const addTodo = useFirestoreCollectionMutation(collectionReference);
@@ -16,7 +16,10 @@ function Home() {
   let content;
   const [title, setTitle] = useState();
   const [description, setDescription] = useState();
-  const [id, setId] = useState();
+  const [id, setId] = useState("");
+  const [editedId, setEditedId] = useState("");
+  const [editedTitle, setEditedTitle] = useState("");
+  const [editedDescription, setEditedDescription] = useState("");
 
   const snapshot = todosQuery.data;
   if (snapshot !== null) {
@@ -107,6 +110,54 @@ function Home() {
           }}
         />
         <button type="submit">Delete Todo</button>
+      </form>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await setDoc(doc(database, "todos", editedId), {
+            title: editedTitle,
+            description: editedDescription,
+          });
+          setEditedTitle("");
+          setEditedDescription("");
+          setEditedId("");
+        }}
+      >
+        <h1>Edit a Todo</h1>
+        <label htmlFor="id">Todo Id:</label>
+        <input
+          type="text"
+          name="id"
+          placeholder="Get todo id to edit"
+          value={editedId}
+          required
+          onChange={(e) => {
+            setEditedId(e.target.value);
+          }}
+        />
+        <label htmlFor="id">Todo Title:</label>
+        <input
+          type="text"
+          name="id"
+          placeholder="Edit todo title"
+          value={editedTitle}
+          required
+          onChange={(e) => {
+            setEditedTitle(e.target.value);
+          }}
+        />
+        <label htmlFor="id">Todo Description:</label>
+        <input
+          type="text"
+          name="id"
+          placeholder="Edit todo description"
+          value={editedDescription}
+          required
+          onChange={(e) => {
+            setEditedDescription(e.target.value);
+          }}
+        />
+        <button type="submit">Edit Todo</button>
       </form>
     </div>
   );
