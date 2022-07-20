e094b98a312e5c11b188a05d9b4113644a1867db
Raymond Brown <daimyoplays@gmail.com>
2022-07-19 20:26:34 -0500

    stuff
    yee

---
+++ project/src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUBAEfCqnDXLI488JDJmuyQrKcQOWoM3w",
+  authDomain: "todo-project-60b73.firebaseapp.com",
+  projectId: "todo-project-60b73",
+  storageBucket: "todo-project-60b73.appspot.com",
+  messagingSenderId: "176991439488",
+  appId: "1:176991439488:web:eec21d984628d2b5cef465",
+};
+
+// Intialize the application
+const app = initializeApp(firebaseConfig);
+
+// Intialize the services
+const database = getFirestore();
+
+// Collection Reference
+export const collectionReference = collection(database, "todos");
